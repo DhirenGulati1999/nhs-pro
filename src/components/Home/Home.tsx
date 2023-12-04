80a57f52974b3aa864fa61426338c1fade3a56bd
pmaithani <pmaithani@deznu.com>
2023-12-04 16:07:35 +0530

    added search box on home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,24 @@
 import SearchBox from "../SearchBox";
+import { useRouter } from "next/router";
 import "../../styles/Home.module.css";
 
 /* eslint-disable react/no-unescaped-entities */
 export const Home = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    // Handle button click logic here
+    console.log("Button clicked!");
+    // Redirect to a new page
+    router.push("/armls/sso/startsso");
+  };
   return (
     <div
       className="main-div"
       id="homesearch"
       style={{ backgroundImage: "url(/homepage_search.jpg)" }}
     >
+      <button onClick={handleClick}>ARMLS Login</button>
       <SearchBox />
       <div>Banner</div>
       <div>Learn how to Sell and Show New Homes</div>
